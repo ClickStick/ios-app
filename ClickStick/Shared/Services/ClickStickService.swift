d6e625421f39acfa3095ed4d224a91c5b931bc1e
Igor Kulman <igor@kulman.sk>
2026-01-23 14:16:42 +0100

    Remove Bluetooth error on device discovery

--- ClickStick/Shared/Services/ClickStickService.swift
+++ ClickStick/Shared/Services/ClickStickService.swift
@@ -72,6 +72,14 @@ final class ClickStickService: CSManagerDelegate {
 
     func didDiscover(device: CSDevice, in manager: CSManager) {
         log.debug("Discovered device: \(device.uuid)")
+        // If we're discovering devices, we must be scanning
+        if !isScanning {
+            isScanning = true
+        }
+        // Clear any previous Bluetooth error since discovery means BT is working
+        if bluetoothError != nil {
+            bluetoothError = nil
+        }
         syncDevicesFromManager()
     }
 
